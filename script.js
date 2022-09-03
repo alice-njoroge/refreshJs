68bb6e09b2ab8682b543898ccfb94144f61e3e71
alice-njoroge <alicenjoroge707@gmail.com>
2022-09-03 15:54:42 +0300

    function chaining with a palindrome and sorting arrays

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ document.write("I am here");
 console.log("it worked!");
 
 let animals = ["cat", "rabbit", "bird", "fish", "chicken"];
+
+//sort in reversing order
+let sortedArray = animals.sort();
+console.log(sortedArray);
+console.log(sortedArray.reverse());
+
+//index guessing game
 let randomIndex = Math.floor(Math.random() * animals.length);
 let randomAnimal = animals[randomIndex];
 let userNumber = prompt("what position do you think " + randomAnimal + " is?");
 let response = userNumber == randomIndex ? "you are right!" : "Wrong! the correct value is " + randomIndex ;
 alert(response);
+
+//find out if text entered is a palindrome or not
+let userValue = prompt("please key in the word");
+let reversedString = userValue.split("").reverse().join('');
+userValue === reversedString ?  alert("This is a palindrome") : alert("Sorry, this is not a palindrome");
+
+
 /*
 let task = ["work", "take trash out","write JS", "take breakfast", "go home"];
 let progress = ["done", "in progress", "to do"];
