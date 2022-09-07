21e3a7b6001fb87447532e4b9243e41e4cdb6538
alice-njoroge <alicenjoroge707@gmail.com>
2022-09-07 03:01:48 +0300

    nested objects

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 
 document.write("I am here");
-//Alert
-//alert("Hello ");
-//console
-console.log("it worked!");
+//objects and loops
+let car = {color:"red", make:"ford", doors:5, year:2020};
+for (let property in car ){
+    console.log(property + ":" + car[property]);
+}
+//Nested objects
+ let bob = {hair:"black",age:20 , eyes:"brown", height:54, job:"development"};
+let friends = {friend1:bob, friend2: {hair:"brown", eyes:"blue", age:30} }
+let friends1 = [bob, {hair:"brown", eyes:"blue"}];
 
+console.log("Hello there " + friends1[1].eyes); //retrieve info on nested arrays objects
+
+//Arrays
 let animals = ["cat", "rabbit", "bird", "fish", "chicken"];
 
 //sort in reversing order
-let sortedArray = animals.sort();
+let sortedArray = animals.sort().reverse();
 console.log(sortedArray);
-console.log(sortedArray.reverse());
 
 //index guessing game
 let randomIndex = Math.floor(Math.random() * animals.length);
@@ -24,8 +31,7 @@ let userValue = prompt("please key in the word");
 let reversedString = userValue.split("").reverse().join('');
 userValue === reversedString ?  alert("This is a palindrome") : alert("Sorry, this is not a palindrome");
 
-
-/*
+//arrays in JS
 let task = ["work", "take trash out","write JS", "take breakfast", "go home"];
 let progress = ["done", "in progress", "to do"];
 
@@ -36,10 +42,11 @@ console.log("today you will " + task[(task.length - 1)] + "status is " + progres
 task[task.length] = "eat banana";
 console.log(task);
 
+//strings in JS
 let t8= "hello world\'s"
 let t9 = "my fraction is 5/8"
-
 console.log(t8, t9);
+
 let q1 = "what is ";
 let q2 = "your name";
 let question = q1 + q2 + "?";
@@ -49,4 +56,4 @@ alert("Hello"+ " "+ myName);
 let firstNumber = prompt("first number?");
 let secondNumber = prompt("second number?");
 let result = Number(firstNumber) + Number(secondNumber);
-alert("your result is " + result); */
+alert("your result is " + result);
