4680c2d5f2f778725204c64daf2d6c877e322631
alice-njoroge <alicenjoroge707@gmail.com>
2022-10-12 19:17:38 +0300

    for in

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-//backward loop
-
-let users = ["larry", "mike", "john"];
-
-for (let i= users.length-1; i>= 0; i--){
-    console.log(users[i])
+// for in
+let userNames = ["larry", "mike"];
+let output = document.getElementById('output');
+for (let v of userNames) {
+    output.innerHTML += v + "<br>";
 }
