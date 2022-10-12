18b9dc4f1d25eadb3744fb1584154f959a0bc02d
alice-njoroge <alicenjoroge707@gmail.com>
2022-10-12 19:04:39 +0300

    guessing game

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-//nested loops -- building a table
-let output = document.getElementById('output');
-let sum = 0; // initialization should be done outside the loop so you dont reset the values
-//
-for (let i = 0; i < 100; i++) {
-    if (i % 5 === 0) {
-        sum += i;
-        output.innerHTML += "value is" + i + "sum= " + sum + "<br>";
-    }
-}
+//backward loop
 
-//fast way of doing the same
-let summation = 0
-for (let i = 0; i < 100; i++) {
-    if (i % 5 !== 0) {
-        continue;
-    }
-    summation += i;
-    output.innerHTML += summation + "<br>";
+let users = ["larry", "mike", "john"];
+
+for (let i= users.length-1; i>= 0; i--){
+    console.log(users[i])
 }
