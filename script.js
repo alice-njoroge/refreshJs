1c779aedd3ee0c6cf340f1c90c183aaa015aa9ba
alice-njoroge <alicenjoroge707@gmail.com>
2022-10-12 19:28:39 +0300

    for in

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // for in
 let userNames = ["larry", "mike"];
 let output = document.getElementById('output');
-for (let v of userNames) {
+for ( let v of userNames) {
     output.innerHTML += v + "<br>";
 }
