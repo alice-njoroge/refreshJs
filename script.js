921d4082687d629f8f36d8c357facc43c7a8334b
alice-njoroge <alicenjoroge707@gmail.com>
2022-10-13 19:05:18 +0300

    local variables

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// functions
-function myFunction(){
-    alert("hello")
+// function variables
+let output = document.getElementById("output");
+
+function myFunction() {
+   let a = "hello ";
+   let b = "world";
+    return  output.innerHTML = a + b;
 }
+
+myFunction();
