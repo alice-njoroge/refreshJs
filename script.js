8dac56144832f219426da18ae6e927c604eca6db
alice-njoroge <alicenjoroge707@gmail.com>
2022-10-25 19:37:56 +0300

    code cracking games with functions

--- script.js
+++
@@ -1,6 +0,0 @@
-// for in
-let userNames = ["larry", "mike", "john", "frank"];
-let output = document.getElementById('output');
-for ( let v in userNames) {
-    output.innerHTML += userNames[v] + "<br>";
-}
